7d67c45368edcd9662e6cb042575cf3e6e2a6da6
bocon3 <boconmarkangelo778@gamil.com>
2024-10-09 17:45:04 +0800

    added logData and ClearLogs

---
+++ applet-1.js
@@ -0,0 +1,38 @@
+class DataLogger {
+    constructor(buttonId, cardContainerId, clearButtonId, logCountId) {
+        this.logButton = document.getElementById(buttonId);
+        this.cardContainer = document.getElementById(cardContainerId);
+        this.clearButton = document.getElementById(clearButtonId);
+        this.logCountElement = document.getElementById(logCountId); 
+        this.loggedData = [];
+
+        this.logButton.addEventListener('click', () => this.logData());
+        this.clearButton.addEventListener('click', () => this.clearLogs());
+    }
+
+    logData() {
+        const timestamp = new Date().toLocaleString();
+        this.loggedData.push(timestamp);
+        this.updateCardContainer();
+    }
+
+    clearLogs() {
+        this.loggedData = []; 
+        this.updateCardContainer(); 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
