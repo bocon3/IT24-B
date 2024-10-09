3b051cbc73abaddb34bc3a3eddbb2fe30444d4da
bocon3 <boconmarkangelo778@gamil.com>
2024-10-09 17:46:46 +0800

    added countsLogs and displayLogCount

--- applet-1.js
+++ applet-1.js
@@ -40,6 +40,16 @@ class DataLogger {
         this.displayLogCount();
     }
 
+    countLogs() {
+        return this.loggedData.length;
+    }
+
+    displayLogCount() {
+        const logCount = this.countLogs();
+        this.logCountElement.innerHTML = `<p>Total Logs: ${logCount}</p>`; 
+    }
+
+
 
 
 
