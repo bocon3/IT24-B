20099be9ef09a1025cc94ad73a09021c461f530b
bocon3 <boconmarkangelo778@gmail.com>
2024-10-16 10:30:07 +0800

    added renderStudentlist and bindSearchEvent

--- applet-3.js
+++ applet-3.js
@@ -21,9 +21,26 @@ class StudentList {
         }
     }
 
-  
+    renderStudentList(students) {
+        const studentListContainer = document.getElementById('studentList');
+        studentListContainer.innerHTML = students.map(student => 
+            `<button class="btn btn-primary" style="margin-top:15px; 
+                                                    width:25rem">
+                ${student.student_name} | ${student.student_program}
+            </button><br>`
+        ).join('');
+    }
 
-  
+    bindSearchEvent() {
+        const studentSearchBar = document.getElementById('studentSearchBar');
+        const studentSearchListContainer = document.getElementById('studentSearchList');
+
+        studentSearchBar.addEventListener('input', () => {
+            this.filterStudents(studentSearchBar.value, studentSearchListContainer);
+        });
+
+        this.renderStudentList(this.students, studentSearchListContainer);
+    }
 
   
 }
